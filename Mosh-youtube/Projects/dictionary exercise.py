a2e24d835f130eb45acb5602b2d80c9fdedffd07
tylertamalunas <tylertamalunas@gmail.com>
2019-12-13 18:30:31 -0600

    starting writing some code, but will need to change

--- Mosh-youtube/Projects/dictionary exercise.py
+++ Mosh-youtube/Projects/dictionary exercise.py
@@ -1,3 +1,28 @@
 # write code that will take user input number and convert to string words
 # Phone: 1234
 # output: One Two Three Four
+
+phone_number = input("Phone: ")
+numb_to_word = {
+    "1": "One",
+    "0": "Zero",
+    "2": "Two",
+    "3": "Three",
+    "4": "Four",
+    "5": "Five",
+    "6": "Six",
+    "7": "Seven",
+    "8": "Eight",
+    "9": "Nine"
+}
+
+print(numb_to_string.get("Zero"))
+print(numb_to_string.get("One"))
+print(numb_to_string.get("Two"))
+print(numb_to_string.get("Three"))
+print(numb_to_string.get("Four"))
+print(numb_to_string.get("Five"))
+print(numb_to_string.get("Six"))
+print(numb_to_string.get("Seven"))
+print(numb_to_string.get("Eight"))
+print(numb_to_string.get("Nine"))
