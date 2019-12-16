b829d84a30d8c7bbd405935da394444b38184ba2
tylertamalunas <tylertamalunas@gmail.com>
2019-12-16 13:04:05 -0600

    finished code to run correctly and added comments to explain what the code does

--- Mosh-youtube/Projects/dictionary exercise.py
+++ Mosh-youtube/Projects/dictionary exercise.py
@@ -2,8 +2,10 @@
 # Phone: 1234
 # output: One Two Three Four
 
+# make a variable to record user input
 phone_number = input("Phone: ")
-numb_to_word = {
+# dictionary that will convert a number into the word
+numb_to_string = {
     "1": "One",
     "0": "Zero",
     "2": "Two",
@@ -15,14 +17,12 @@ numb_to_word = {
     "8": "Eight",
     "9": "Nine"
 }
+# variable that will hold the converted numbers
+output = ""
+# loop that goes through each character in the variable "phone_number"
+for character in phone_number:
+    # converts the number to its word, and adds it into the variable "output". Adds a space between each one and puts
+    # an ! if there is something other than a number input
+    output += numb_to_string.get(character, "!") + " "
+print(output)
 
-print(numb_to_string.get("Zero"))
-print(numb_to_string.get("One"))
-print(numb_to_string.get("Two"))
-print(numb_to_string.get("Three"))
-print(numb_to_string.get("Four"))
-print(numb_to_string.get("Five"))
-print(numb_to_string.get("Six"))
-print(numb_to_string.get("Seven"))
-print(numb_to_string.get("Eight"))
-print(numb_to_string.get("Nine"))
