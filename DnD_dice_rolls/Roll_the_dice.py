058d26bb65f8a537a364421e84a64565bc16f553
tylertamalunas <tylertamalunas@gmail.com>
2019-12-28 17:30:14 -0600

    Started new project for making different DnD dice. Directory of DnD_dice_rolls. Still need to outline how it will be set up.

---
+++ DnD_dice_rolls/Roll_the_dice.py
@@ -0,0 +1,9 @@
+"""
+This is the file to call upon when rolling dice
+User inputs which dice they are rolling and how many, and then this calls the correct function to roll
+the dice and show the player their roll.
+"""
+import dice_roll
+
+number_of_dice = input("Number of dice: ")
+dice_sides = input("Amount of sides: ")
---
+++ DnD_dice_rolls/Roll_types/dice_roll.py
@@ -0,0 +1,10 @@
+"""
+module that holds the different types of dice rolls.
+"""
+
+import random
+
+
+class Dice:
+    def sides20(self):  # Should this just be for one dice or also include 2+?
+
