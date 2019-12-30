5e5872b4e2aae4099ffaa1247b0cdce78967c0f7
tylertamalunas <tylertamalunas@gmail.com>
2019-12-30 17:26:27 -0600

    Trying to figure out how to create a package/module and import the class/function into another file.

--- DnD_dice_rolls/Roll_the_dice.py
+++ DnD_dice_rolls/Roll_the_dice.py
@@ -3,7 +3,13 @@ This is the file to call upon when rolling dice
 User inputs which dice they are rolling and how many, and then this calls the correct function to roll
 the dice and show the player their roll.
 """
-import dice_roll
+import random
+from Roll_types.dice_roll import Dice
 
 number_of_dice = input("Number of dice: ")
 dice_sides = input("Amount of sides: ")
+
+
+dice = Dice.sides20()
+if dice_sides == 20:
+    print(dice)
--- DnD_dice_rolls/Roll_types/dice_roll.py
+++ DnD_dice_rolls/Roll_types/dice_roll.py
@@ -1,10 +1,22 @@
 """
 module that holds the different types of dice rolls.
 """
-
 import random
+
+number_of_dice = input("Number of dice: ")
+dice_sides = input("Amount of sides: ")
 
 
 class Dice:
     def sides20(self):  # Should this just be for one dice or also include 2+?
+        if number_of_dice == 2:
+            first_roll = random.randint(1, 20)
+            second_roll = random.randint(1, 20)
+            return first_roll, second_roll
+        else:
+            first_roll = random.randint(1, 20)
+            return first_roll
 
+
+dice = Dice()
+print(dice.sides20())
