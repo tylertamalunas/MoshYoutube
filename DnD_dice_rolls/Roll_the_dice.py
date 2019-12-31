842264e386f0fcba6c4381ead778335e8b25ca82
tylertamalunas <tylertamalunas@gmail.com>
2019-12-31 13:07:54 -0600

    Finished making functions for each dice. could be made into smaller code if needed, but wanted to try it with classes and functions.

--- DnD_dice_rolls/Roll_the_dice.py
+++ DnD_dice_rolls/Roll_the_dice.py
@@ -5,21 +5,70 @@ D20, D12, D10, D8, D6, D4
 """
 import random
 
-number_of_dice = input("Number of dice: ")
-dice_sides = input("Amount of sides: ")
+number_of_dice = int(input("Number of dice: "))
+dice_sides = int(input("Amount of sides: "))
 
 
 class Dice:
+    def d4(self):
+        roll_number = 1
+        rolls = []
+        while roll_number <= number_of_dice:
+            roll_number += 1
+            rolls.append(random.randint(1, dice_sides))
+        return rolls
+
+    def d6(self):
+        roll_number = 1
+        rolls = []
+        while roll_number <= number_of_dice:
+            roll_number += 1
+            rolls.append(random.randint(1, dice_sides))
+        return rolls
+
+    def d8(self):
+        roll_number = 1
+        rolls = []
+        while roll_number <= number_of_dice:
+            roll_number += 1
+            rolls.append(random.randint(1, dice_sides))
+        return rolls
+
+    def d10(self):
+        roll_number = 1
+        rolls = []
+        while roll_number <= number_of_dice:
+            roll_number += 1
+            rolls.append(random.randint(1, dice_sides))
+        return rolls
+
+    def d12(self):
+        roll_number = 1
+        rolls = []
+        while roll_number <= number_of_dice:
+            roll_number += 1
+            rolls.append(random.randint(1, dice_sides))
+        return rolls
+
     def d20(self):  # Should this just be for one dice or also include 2+?
-        if number_of_dice == '2':  # maybe change to a while loop, while x <= number_of_dice
-            first_roll = random.randint(1, 20)
-            second_roll = random.randint(1, 20)
-            return first_roll, second_roll
-        else:
-            first_roll = random.randint(1, 20)
-            return first_roll
+        roll_number = 1
+        rolls = []
+        while roll_number <= number_of_dice:
+            roll_number += 1
+            rolls.append(random.randint(1, dice_sides))
+        return rolls
 
 
 dice = Dice()
-if dice_sides == '20':
+if dice_sides == 4:
+    print(dice.d4())
+elif dice_sides == 6:
+    print(dice.d6())
+elif dice_sides == 8:
+    print(dice.d8())
+elif dice_sides == 10:
+    print(dice.d10())
+elif dice_sides == 12:
+    print(dice.d12())
+elif dice_sides == 20:
     print(dice.d20())
