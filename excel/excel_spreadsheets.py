47dd9202524f0ec13d984b350b0262f3e31072a8
tylertamalunas <tylertamalunas@gmail.com>
2020-01-05 16:12:30 -0600

    Project excel. creating program to modify any number of excel sheets at a time. Current commit takes a cell in each row and modifies it, then saves it as another excel sheet.

---
+++ excel/excel_spreadsheets.py
@@ -0,0 +1,19 @@
+# This will be able to automatically process thousands of sheets in under a second
+# uses openpyxl
+
+import openpyxl as xl
+# renaming the function as wb to make it easier
+wb = xl.load_workbook('transactions.xlsx')
+sheet = wb['Sheet1']
+cell = sheet['a1']
+# or
+cell = sheet.cell(1,1)
+
+# for loop to generate the cell numbers, then add the new cell into a different column.
+for row in range(2, sheet.max_row + 1):
+    cell = sheet.cell(row, 3)
+    corrected_price = cell.value * 0.9
+    corrected_price_cell = sheet.cell(row, 4)
+    corrected_price_cell.value = corrected_price
+
+wb.save('transactions2.xlsx')
